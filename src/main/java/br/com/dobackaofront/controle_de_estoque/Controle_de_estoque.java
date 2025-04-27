d314307b416c64db7fdf0f2cefe1d55992ed4943
Olival Paulino <olivalpaulinocc@gmail.com>
2025-04-27 10:11:21 -0300

    inicializacao do projeto

---
+++ src/main/java/br/com/dobackaofront/controle_de_estoque/Controle_de_estoque.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.dobackaofront.controle_de_estoque;
+
+/**
+ *
+ * @author Família Santos
+ */
+public class Controle_de_estoque {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
